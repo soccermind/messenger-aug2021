f5c4f6668c9586bf4568f2fe318b878e7c1ae212
Alberto Olvera Sotres <alberto.olvera.sotres@gmail.com>
2021-08-17 17:12:19 -0400

    Added missing findConversation method to the conversation model

--- server/db/models/conversation.js
+++ server/db/models/conversation.js
@@ -1,6 +1,7 @@
 const { Op } = require("sequelize");
 const db = require("../db");
 const Message = require("./message");
+const Conversation_User = require("./conversationUser")
 
 const Conversation = db.define("conversation", {
   createdByUserId: {
@@ -8,6 +9,19 @@ const Conversation = db.define("conversation", {
     allowNull: false,
   },
 });
+
+Conversation.findConversation = async function (participantIds) {
+  const conversation = await Conversation.findOne({
+    include: { 
+      model: Conversation_User,
+      where: {
+        userId: {
+          [Op.in]: participantIds
+        }
+      }
+    }
+  });
+};
 
 
 module.exports = Conversation;
